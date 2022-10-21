802608b51be26af52a26a5685474f8f43fbbad4d
Sadnan-2019 <54290526+Sadnan-2019@users.noreply.github.com>
2022-10-22 01:12:47 +0600

    nav

--- src/Components/Landing/Landing.js
+++ src/Components/Landing/Landing.js
@@ -64,7 +64,7 @@ const Landing = () => {
           <div className=" ">
             <div className=" md:py-20 py-10 lg:py-24 lg:mx-16 mx-8 grid lg:grid-cols-1 grid-cols-1 gap-5">
               <div className="sm:max-w-xs lg:max-w-xl text-left md:ml-8 lg:order-1 order-2   lg:mt-0">
-                <div className="hidden lg:flex items-center gap-2   lg:bg-gray-100 w-fit px-3 rounded-full py-2 border-[1px]  lg:border-gray-200 mb-5">
+                {/* <div className="hidden lg:flex items-center gap-2   lg:bg-gray-100 w-fit px-3 rounded-full py-2 border-[1px]  lg:border-gray-200 mb-5">
                   <p
                     className="hidden lg:block px-2 py-[2px] text-white lg:rounded-full  "
                     style={{ backgroundColor: "#453364" }}
@@ -74,7 +74,7 @@ const Landing = () => {
                   <p className="hidden lg:block text-sm ">
                     Discover the easiest way to get our service!
                   </p>
-                </div>
+                </div> */}
                 <p
                   data-aos="fade-up"
                   data-aos-duration="1000"
@@ -90,12 +90,10 @@ const Landing = () => {
                   className="max-w-lg mt-7 font-bold   md:text-[25px] poppins-t    "
                   style={{ lineHeight: "120%", color: "#CB052F" }}
                 >
-                  Our highly qualified and experienced teams use the most
-                  up-to-date techniques to swiftly and smoothly repair your
-                  body, with the goal of long-term wellness
+                  Be seen by a Consultant expert in as little as 24 hours
                 </p>
 
-                <div className="flex items-center mt-2 font-bold">
+                {/* <div className="flex items-center mt-2 font-bold">
                   <FaPhoneVolume
                     className="lg:text-2xl text-[20px] "
                     style={{ color: "#CB052F" }}
@@ -107,7 +105,7 @@ const Landing = () => {
                     {" "}
                     Call Now: 01619848555
                   </span>
-                </div>
+                </div> */}
 
                 <div
                   data-aos="fade-up"
@@ -119,7 +117,7 @@ const Landing = () => {
                     to="/"
                     style={{ backgroundColor: "#CB052F", color: "white" }}
                   >
-                    <span className="text-sm font-medium"> Get Started </span>
+                    <span className="text-sm font-medium"> More</span>
                     <BsArrowRight className="text-lg ml-2" />
                   </Link>
                 </div>
--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -1,53 +1,184 @@
-import React from 'react';
+import React from "react";
+import { NavLink } from "react-router-dom";
 
 const Nav = () => {
-     return (
-          <div>
-              <div className="navbar bg-red-500" style={{}}>
-  <div className="navbar-start">
-    <div className="dropdown">
-      <label tabIndex={0} className="btn btn-ghost lg:hidden">
-        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
-      </label>
-      <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-        <li><a>Item 1</a></li>
-        <li tabIndex={0}>
-          <a className="justify-between">
-            Parent
-            <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z"/></svg>
-          </a>
-          <ul className="p-2">
-            <li><a>Submenu 1</a></li>
-            <li><a>Submenu 2</a></li>
-          </ul>
-        </li>
-        <li><a>Item 3</a></li>
-      </ul>
-    </div>
-    <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
-  </div>
-  <div className="navbar-center hidden lg:flex">
-    <ul className="menu menu-horizontal p-0">
-      <li><a>Item 1</a></li>
-      <li tabIndex={0}>
-        <a>
-          Parent
-          <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z"/></svg>
-        </a>
-        <ul className="p-2">
-          <li><a>Submenu 1</a></li>
-          <li><a>Submenu 2</a></li>
-        </ul>
+  const active =
+    "text-white mx-1 font-medium border-b-2 border-white-900 pb-1 md:text-white-900 ";
+  const normal =
+    "  mx-1 text-white-600 font-medium font-bold focus:text-white p-0 lg:text-white   ";
+
+  const navItem = (
+    <>
+      <li className="nav-item p-2 	">
+        <NavLink
+          className={({ isActive }) => (isActive ? `${active}` : `${normal}`)}
+          to="/home"
+        >
+          Home
+        </NavLink>
       </li>
-      <li><a>Item 3</a></li>
-    </ul>
-  </div>
-  <div className="navbar-end">
-    <a className="btn">Get started</a>
-  </div>
-</div> 
+
+      <li className="nav-item p-2">
+        <NavLink
+          className={({ isActive }) => (isActive ? `${active}` : `${normal}`)}
+          to="/physiotherapy"
+        >
+          Physiotherapy
+        </NavLink>
+      </li>
+      <li className="nav-item p-2 	">
+        <NavLink
+          className={({ isActive }) => (isActive ? `${active}` : `${normal}`)}
+          to="/spinal-pain"
+        >
+          Spinal Pain
+        </NavLink>
+      </li>
+      <li className="nav-item p-2 	">
+        <NavLink
+          className={({ isActive }) => (isActive ? `${active}` : `${normal}`)}
+          to="/nursing"
+        >
+          Pre & Post Operative Rehabilitation
+        </NavLink>
+      </li>
+      {/* <li className="nav-item p-2">
+              <NavLink
+                className={({ isActive }) => (isActive ? `${active}` : `${normal}`)}
+                to="/medical-assistant"
+              >
+                Medical Assistant
+              </NavLink>
+            </li>
+            <li className="nav-item p-2">
+              <NavLink
+                className={({ isActive }) => (isActive ? `${active}` : `${normal}`)}
+                to="/medical-caregiver"
+              >
+                Medical Caregiver
+              </NavLink>
+            </li>
+            <li className="nav-item p-2">
+              <NavLink
+                className={({ isActive }) => (isActive ? `${active}` : `${normal}`)}
+                to="/caregiver"
+              >
+                Caregiver
+              </NavLink>
+            </li>
+            
+            <li className="nav-item p-2">
+              <NavLink
+                className={({ isActive }) => (isActive ? `${active}` : `${normal}`)}
+                to="/nanycare"
+              >
+                Nanycare
+              </NavLink>
+            </li>
+            <li className="nav-item p-2">
+              <NavLink
+                className={({ isActive }) => (isActive ? `${active}` : `${normal}`)}
+                to="/companion"
+              >
+                Companionship 
+              </NavLink>
+            </li>
+            <li className="nav-item p-2">
+              <NavLink
+                className={({ isActive }) => (isActive ? `${active}` : `${normal}`)}
+                to="/medical-equipment"
+              >
+                Medical Equipment
+              </NavLink>
+            </li> */}
+
+      {/* <li className="dropdown dropdown-hover hover:border-none" style={{ backgroundColor: "#453364" }} > 
+             
+        <div className="flex items-center">
+        <label tabIndex={0} className="m-1 text-white" style={{ backgroundColor: "#453364" }}>More</label>
+        <svg
+                  class="fill-current h-4 w-4 text-white"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                >
+                  <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />{" "}
+                </svg>
+        </div>
+        
+        
+        
+        <ul tabIndex={0} className="w-24 dropdown-content menu p-2 shadow  rounded-box lg:w-52" style={{ backgroundColor: "#453364" }}>
+        <li className="nav-item p-2">
+              <NavLink
+                className={({ isActive }) => (isActive ? `${active}` : `${normal}`)}
+                to="/blog"
+              >
+                Blog
+              </NavLink>
+            </li>
+            <li className="nav-item p-2">
+              <NavLink
+                className={({ isActive }) => (isActive ? `${active}` : `${normal}`)}
+                to="/about"
+              >
+                About
+              </NavLink>
+            </li>
+            <li className="nav-item p-2">
+              <NavLink
+                className={({ isActive }) => (isActive ? `${active}` : `${normal}`)}
+                to="/contact"
+              >
+                Contact
+              </NavLink>
+            </li>
+        </ul>
+       
+      
+      
+            </li>
+       */}
+    </>
+  );
+  return (
+    <div>
+      <div className="navbar  " style={{ backgroundColor: "#CB052F" }}>
+        <div className="navbar-start">
+          <div className="dropdown">
+            <label tabIndex={0} className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />
+              </svg>
+            </label>
+            <ul
+              tabIndex={0}
+              className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+            >
+              {navItem}
+            </ul>
           </div>
-     );
+          <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
+        </div>
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal p-0">{navItem}</ul>
+        </div>
+        <div className="navbar-end">
+          <a className="btn">Get started</a>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Nav;
