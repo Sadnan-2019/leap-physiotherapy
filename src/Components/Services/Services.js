4aac13085603f57007bcc2f6520f62fcc0c5d95b
Sadnan-2019 <sadnan.wow@gmail.com>
2022-10-22 15:26:37 +0600

    inmg

--- src/Components/Services/Services.js
+++ src/Components/Services/Services.js
@@ -12,66 +12,27 @@ const Services = () => {
 
 <div className='grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2  mx-10 gap-10    py-8'>
 
-<div className=" rounded-lg     flex items-center justify-center	 gap-5      shadow-lg shadow-white-1000/50 border   " style={{backgroundColor:"white",borderRadius:"25px"}}>
-  <figure><img src="https://www.rehabphysiolondon.com/wp-content/uploads/2019/06/Neuro.png" alt="Movie"/></figure>
+<div className="      flex items-center justify-center	 gap-5      shadow-lg shadow-white-1000/50 border   p-10" style={{backgroundColor:"white",borderRadius:"25px"}}>
+  <img src="https://www.rehabphysiolondon.com/wp-content/uploads/2019/06/Neuro.png " className='w-32' alt="Movie"/> 
   <div className=" ">
-    <h2 className=" ">New movie is released!</h2>
-    <p>Click the button to watch on Jetflix app.</p>
+    <h2 className=" ">Neuro Rehab</h2>
+    <p>Focused on helping conditions of the nervous system</p>
     <div className=" ">
-      <button className="btn btn-primary">Watch</button>
+      <button className="btn btn-primary">Details</button>
     </div>
   </div>
 </div>
-<div className=" rounded-lg     flex items-center gap-5 justify-content-center  bg-base-100 drop-shadow-[0_35px_35px_rgba(0,0,0,0.25)]   ">
-  <figure><img src="https://www.rehabphysiolondon.com/wp-content/uploads/2019/06/Neuro.png" alt="Movie"/></figure>
-  <div className="card-body">
-    <h2 className="card-title">New movie is released!</h2>
-    <p>Click the button to watch on Jetflix app.</p>
-    <div className="card-actions justify-end">
-      <button className="btn btn-primary">Watch</button>
+<div className="      flex items-center justify-center	 gap-5      shadow-lg shadow-white-1000/50 border   p-10" style={{backgroundColor:"white",borderRadius:"25px"}}>
+  <img src="https://www.rehabphysiolondon.com/wp-content/uploads/2019/06/Respiratory.png" className='w-32' alt="Movie"/> 
+  <div className=" ">
+    <h2 className=" ">Chest Physio</h2>
+    <p>Respiratory physiotherapy helps treat, educate and advise people who have problems with their lungs and heart</p>
+    <div className=" ">
+      <button className="btn btn-primary">Details</button>
     </div>
   </div>
 </div>
-{/* <div className="card card-side bg-base-100 shadow-xl">
-  <figure><img src="https://placeimg.com/200/280/arch" alt="Movie"/></figure>
-  <div className="card-body">
-    <h2 className="card-title">New movie is released!</h2>
-    <p>Click the button to watch on Jetflix app.</p>
-    <div className="card-actions justify-end">
-      <button className="btn btn-primary">Watch</button>
-    </div>
-  </div>
-</div>
-<div className="card card-side bg-base-100 shadow-xl">
-  <figure><img src="https://placeimg.com/200/280/arch" alt="Movie"/></figure>
-  <div className="card-body">
-    <h2 className="card-title">New movie is released!</h2>
-    <p>Click the button to watch on Jetflix app.</p>
-    <div className="card-actions justify-end">
-      <button className="btn btn-primary">Watch</button>
-    </div>
-  </div>
-</div>
-<div className="card card-side bg-base-100 shadow-xl">
-  <figure><img src="https://placeimg.com/200/280/arch" alt="Movie"/></figure>
-  <div className="card-body">
-    <h2 className="card-title">New movie is released!</h2>
-    <p>Click the button to watch on Jetflix app.</p>
-    <div className="card-actions justify-end">
-      <button className="btn btn-primary">Watch</button>
-    </div>
-  </div>
-</div>
-<div className="card card-side bg-base-100 shadow-xl">
-  <figure><img src="https://placeimg.com/200/280/arch" alt="Movie"/></figure>
-  <div className="card-body">
-    <h2 className="card-title">New movie is released!</h2>
-    <p>Click the button to watch on Jetflix app.</p>
-    <div className="card-actions justify-end">
-      <button className="btn btn-primary">Watch</button>
-    </div>
-  </div>
-</div> */}
+
 </div>
 
                </div>
