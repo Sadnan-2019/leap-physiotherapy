04cfe56c97258f1e680643abf4139a667c339e71
Sadnan-2019 <54290526+Sadnan-2019@users.noreply.github.com>
2022-10-23 07:21:27 +0600

    nav

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Nav from './Components/Nav/Nav';
 import Home from './Components/Home/Home';
+import Footer from './Components/Footer/Footer';
 
 function App() {
   return (
     <div className=" ">
       <Nav/>
       <Home/>
+      <Footer/>
     </div>
   );
 }
