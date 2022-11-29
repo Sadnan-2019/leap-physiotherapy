de85804196310a7e43036425dab739a910e7c9de
Sadnan-2019 <sadnan.wow@gmail.com>
2022-11-29 15:24:31 +0600

    electror

--- src/Components/Team/Team.js
+++ src/Components/Team/Team.js
@@ -14,7 +14,7 @@ const Team = () => {
             <div class="rounded-xl z-50 opacity-0 group-hover:opacity-100 transition duration-300 ease-in-out cursor-pointer absolute from-black/80 to-transparent bg-gradient-to-t inset-x-0 -bottom-2 pt-30 text-white flex items-end">
               <div>
                 <div class="transform-gpu  p-4 space-y-3 text-xl group-hover:opacity-100 group-hover:translate-y-0 translate-y-4 pb-10 transform transition duration-300 ease-in-out">
-                  <div class="font-bold">Jessie Watsica</div>
+                  <div class="font-bold">Jessie Watsica Bangladesh</div>
 
                   <div class="opacity-60 text-sm ">
                     Lorem ipsum dolor sit amet, consectetur adipisicing elit.
@@ -27,7 +27,7 @@ const Team = () => {
             </div>
             <img
               alt=""
-              class="object-cover  w-full h-56  aspect-square group-hover:scale-110 transition duration-300 ease-in-out"
+              class="sm:w-32 h-20 object-cover  lg:w-full lg:h-56  aspect-square group-hover:scale-110 transition duration-300 ease-in-out"
               src="https://images.unsplash.com/photo-1650790362847-3c1dd609d0c5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=686&q=80
 "
             />
