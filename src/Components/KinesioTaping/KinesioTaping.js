023ec27b5e80a5d6fec3864a9425042c02897ddd
Sadnan-2019 <sadnan.wow@gmail.com>
2022-12-03 17:42:22 +0600

    nav and slider

--- src/Components/KinesioTaping/KinesioTaping.js
+++ src/Components/KinesioTaping/KinesioTaping.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import Review from '../Review/Review';
+import Team from '../Team/Team';
+import KinesioTapingDetails from './KinesioTapingDetails';
 import KinesioTapingLanding from './KinesioTapingLanding';
 
 const KinesioTaping = () => {
     return (
         <div>
             <KinesioTapingLanding></KinesioTapingLanding>
+            <KinesioTapingDetails></KinesioTapingDetails>
+            <Review></Review>
+            <Team></Team>
         </div>
     );
 };
--- src/Components/KinesioTaping/KinesioTapingDetails.js
+++ src/Components/KinesioTaping/KinesioTapingDetails.js
@@ -1,11 +1,145 @@
-import React from 'react';
+import React from "react";
+import { AiOutlineCaretRight } from "react-icons/ai";
+import electrone from "../../assets/electroone.jpg";
+import electrotwo from "../../assets/electrotwo.jpg";
+import electrothree from "../../assets/electrothree.jpg";
+import electrofour from "../../assets/electro4.jpg";
+// import { AiOutlineCaretRight } from "react-icons/ai";
 
 const KinesioTapingDetails = () => {
-    return (
-        <div>
-            
+  return (
+    <div>
+      <div className="grid sm:grid-cols-1 lg:grid-cols-2  justify-center items-center    gap-12  px-10  ">
+        <div className="justify-center items-center">
+          <p className="text-2xl font-medium text-justify py-2 ml-3">
+          Kinesio Taping
+ -
+          </p>
+          <p className="text-sm font-medium text-justify py-2 ml-3">
+            Kinesiology tape is a thin, stretchy, elastic cotton strip with an
+            acrylic adhesive. Therapeutic kinesiology tape that can benefit a
+            wide variety of musculoskeletal and sports injuries, plus
+            inflammatory conditions. Kinesiology tape is almost identical to
+            human skin in both thickness and elasticity, which allows kinesio
+            tape to be worn without binding, constricting or restriction of your
+            movement. Kinesiology tape is an that is used for treating athletic
+            injuries and a variety of physical disorders..
+          </p>
+          <p className="text-2xl font-medium text-justify py-2 ml-3">
+          Kinesio Tape May Help:
+          </p>
+          <div className="flex items-center justify-start">
+            <span>
+              <AiOutlineCaretRight className="text-2xl text-blue-900"></AiOutlineCaretRight>
+            </span>
+            <p className="text-md font-medium text-justify py-2 ml-3">
+            Muscle Strains 
+            </p>
+          </div>
+          <div className="flex items-center justify-start">
+            <span>
+              <AiOutlineCaretRight className="text-2xl text-blue-900"></AiOutlineCaretRight>
+            </span>
+            <p className="text-md font-medium text-justify py-2 ml-3">
+            Ligament Sprains
+
+            </p>
+          </div>
+          <div className="flex items-center justify-start">
+            <span>
+              <AiOutlineCaretRight className="text-2xl text-blue-900"></AiOutlineCaretRight>
+            </span>
+            <p className="text-md font-medium text-justify py-2 ml-3">
+            Bruisingor Haematoma
+
+            </p>
+          </div>
+          <div className="flex items-center justify-start">
+            <span>
+              <AiOutlineCaretRight className="text-2xl text-blue-900"></AiOutlineCaretRight>
+            </span>
+            <p className="text-md font-medium text-justify py-2 ml-3">
+            Muscle Weakness
+            </p>
+          </div>
+          <div className="flex items-center justify-start">
+            <span>
+              <AiOutlineCaretRight className="text-2xl text-blue-900"></AiOutlineCaretRight>
+            </span>
+            <p className="text-md  font-medium text-justify py-2 ml-3">
+            Hypotonia – Low Muscle Tone
+            </p>
+          </div>
+          <div className="flex items-center justify-start">
+            <span>
+              <AiOutlineCaretRight className="text-2xl text-blue-900"></AiOutlineCaretRight>
+            </span>
+            <p className="text-md font-medium text-justify py-2 ml-3">
+            Joint Alignment and Maltracking
+
+            </p>
+          </div>
+          <div className="flex items-center justify-start">
+            <span>
+              <AiOutlineCaretRight className="text-2xl text-blue-900"></AiOutlineCaretRight>
+            </span>
+            <p className="text-md font-medium text-justify py-2 ml-3">
+            Joint instability or Hypermobility
+            </p>
+          </div>
+          <div className="flex items-center justify-start">
+            <span>
+              <AiOutlineCaretRight className="text-2xl text-blue-900"></AiOutlineCaretRight>
+            </span>
+            <p className="text-md font-medium text-justify py-2 ml-3">
+            Poor Posture: Round Shoulders, Slouched Spine
+
+            </p>
+          </div>
+          <div className="flex items-center justify-start">
+            <span>
+              <AiOutlineCaretRight className="text-2xl text-blue-900"></AiOutlineCaretRight>
+            </span>
+            <p className="text-md font-medium text-justify py-2 ml-3">
+            Scapular Instability (SICK)
+
+
+            </p>
+          </div>
+          <div className="flex items-center justify-start">
+            <span>
+              <AiOutlineCaretRight className="text-2xl text-blue-900"></AiOutlineCaretRight>
+            </span>
+            <p className="text-md font-medium text-justify py-2 ml-3">
+            Post-fracturesupport 
+
+
+
+            </p>
+          </div>
+          <div className="flex items-center justify-start">
+            <span>
+              <AiOutlineCaretRight className="text-2xl text-blue-900"></AiOutlineCaretRight>
+            </span>
+            <p className="text-md font-medium text-justify py-2 ml-3">
+            Tendonitis 
+
+
+
+
+            </p>
+          </div>
         </div>
-    );
+
+        <div className=" grid grid-cols-2 gap-8 mt-[50px] py-6 ">
+          <img src={electrone} className="w-64 h-44 rounded-lg" alt="" />
+          <img src={electrotwo} className="w-64 h-44  rounded-lg" alt="" />
+          <img src={electrothree} className="w-64  h-44  rounded-lg" alt="" />
+          <img src={electrofour} className="w-64 h-44 rounded-lg" alt="" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default KinesioTapingDetails;
