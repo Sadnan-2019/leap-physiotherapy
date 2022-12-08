79218bf250bde5d5932bdc85b40146f204ff7827
Sadnan-2019 <sadnan.wow@gmail.com>
2022-12-08 15:54:50 +0600

    services responcive

--- src/Components/Services/Services.js
+++ src/Components/Services/Services.js
@@ -8,7 +8,8 @@ const Services = () => {
         <div className=" ">
           <h2
             className="text-3xl text-center  py-8 font-bold poppins-b sm:text-4xl"
-            style={{ color: "#536E9B" }} data-aos="fade-up"
+            style={{ color: "#536E9B" }}
+            data-aos="fade-up"
             data-aos-duration="1000"
           >
             Our Services
@@ -17,32 +18,37 @@ const Services = () => {
 
         <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 px-12 gap-10    py-[25px] items-center justify-items-center">
           <div
-            className="  bg-gradient-to-r from-[#034ca7]  to-[#5f7499]    sm:w-5/6  lg:w-5/6  flex items-center justify-center	 gap-5      shadow-2xl shadow-white-1000/50 border    p-10"
-
-            
+            className="  bg-gradient-to-r from-[#034ca7] h-3/4  to-[#5f7499]    sm:w-5/6  lg:w-5/6  flex items-center justify-center	 gap-5      shadow-2xl shadow-white-1000/50 border    p-10"
             // data-aos="fade-up-right"
             // data-aos-offset="300"
             // data-aos-delay="5000"
             // data-aos-duration="3000"
             style={{
-              
               borderRadius: "25px",
               color: "white",
-              height: "230px",
             }}
           >
-            <div className=""  data-aos="fade-right"
-            data-aos-offset="300"
-            data-aos-delay="5000"
-            data-aos-duration="3000">
+            <div
+              className=""
+              data-aos="fade-right"
+              data-aos-offset="300"
+              data-aos-delay="5000"
+              data-aos-duration="3000"
+            >
               {" "}
               <img src={physio} className="w-96   lg:h-36 " alt="" />{" "}
             </div>
-            <div className=" text-white"  data-aos="fade-down"
-            data-aos-offset="300"
-            data-aos-delay="5000"
-            data-aos-duration="3000" >
-              <h2 className="tex-sm lg:text-2xl font-bold "> Physiotherapy </h2>
+            <div
+              className=" text-white"
+              data-aos="fade-down"
+              data-aos-offset="300"
+              data-aos-delay="5000"
+              data-aos-duration="3000"
+            >
+              <h2 className="tex-sm   lg:text-2xl font-bold ">
+                {" "}
+                Physiotherapy{" "}
+              </h2>
               <p className="text-xs lg:text-[15px]">
                 Experience the best physiotherapy services at LEAP to improve
                 function,reduce symptoms & prevent future complications.
@@ -58,18 +64,18 @@ const Services = () => {
             </div>
           </div>
           <div
-            className="  bg-gradient-to-r from-[#034ca7]  to-[#5f7499]  sm:w-5/6  lg:w-5/6     flex items-center justify-center	 gap-5      shadow-lg shadow-white-1000/50 border   p-10"
+            className="  bg-gradient-to-r from-[#034ca7]  to-[#5f7499] h-3/4   sm:w-5/6  lg:w-5/6     flex items-center justify-center	 gap-5      shadow-lg shadow-white-1000/50 border   p-10"
             style={{
               backgroundColor: "white",
               borderRadius: "25px",
-            
-              height: "230px",
             }}
           >
-            <div  data-aos="fade-right"
-            data-aos-offset="300"
-            data-aos-delay="5000"
-            data-aos-duration="3000">
+            <div
+              data-aos="fade-right"
+              data-aos-offset="300"
+              data-aos-delay="5000"
+              data-aos-duration="3000"
+            >
               {" "}
               <img
                 src="https://www.rehabphysiolondon.com/wp-content/uploads/2019/06/Neuro.png   "
@@ -77,18 +83,21 @@ const Services = () => {
                 alt=""
               />{" "}
             </div>
-            <div className="text-white "  data-aos="fade-down"
-            data-aos-offset="300"
-            data-aos-delay="5000"
-            data-aos-duration="3000">
+            <div
+              className="text-white "
+              data-aos="fade-down"
+              data-aos-offset="300"
+              data-aos-delay="5000"
+              data-aos-duration="3000"
+            >
               <h2 className="tex-sm lg:text-2xl font-bold ">Rehabilitation</h2>
               <p className=" text-xs lg:text-[15px]">
-                Rehabilitation   aims to improve function, reduce symptoms
-                and ascertain the wellbeing of people with complex injuries and
+                Rehabilitation aims to improve function, reduce symptoms and
+                ascertain the wellbeing of people with complex injuries and
                 conditions affecting the brain and nervous system.
               </p>
               <div className="card-actions justify-end  mt-5">
-              <button
+                <button
                   className="btn btn-sm  "
                   style={{ backgroundColor: "#3E84DC" }}
                 >
@@ -98,18 +107,17 @@ const Services = () => {
             </div>
           </div>
           <div
-            className=" bg-gradient-to-r from-[#034ca7]  to-[#5f7499]    sm:w-5/6  lg:w-5/6     flex items-center justify-center	 gap-5      shadow-lg shadow-white-1000/50 border   p-10"
+            className=" bg-gradient-to-r from-[#034ca7]  to-[#5f7499]  h-3/4   sm:w-5/6  lg:w-5/6     flex items-center justify-center	 gap-5      shadow-lg shadow-white-1000/50 border   p-10"
             style={{
-              
               borderRadius: "25px",
-               
-              height: "230px",
             }}
           >
-            <div  data-aos="fade-right"
-            data-aos-offset="300"
-            data-aos-delay="5000"
-            data-aos-duration="3000" >
+            <div
+              data-aos="fade-right"
+              data-aos-offset="300"
+              data-aos-delay="5000"
+              data-aos-duration="3000"
+            >
               {" "}
               <img
                 src="https://www.rehabphysiolondon.com/wp-content/uploads/2019/06/Respiratory.png "
@@ -117,17 +125,20 @@ const Services = () => {
                 alt=""
               />{" "}
             </div>
-            <div className=" text-white"  data-aos="fade-down"
-            data-aos-offset="300"
-            data-aos-delay="5000"
-            data-aos-duration="3000">
+            <div
+              className=" text-white"
+              data-aos="fade-down"
+              data-aos-offset="300"
+              data-aos-delay="5000"
+              data-aos-duration="3000"
+            >
               <h2 className="tex-sm lg:text-2xl font-bold">Chest Physio</h2>
               <p className="text-xs lg:text-[15px]">
                 Respiratory physiotherapy helps treat, educate and advise people
                 who have problems with their lungs and heart.
               </p>
               <div className=" card-actions justify-end  mt-5">
-              <button
+                <button
                   className="btn btn-sm  "
                   style={{ backgroundColor: "#3E84DC" }}
                 >
@@ -136,19 +147,19 @@ const Services = () => {
               </div>
             </div>
           </div>
+
           <div
-            className=" bg-gradient-to-r from-[#034ca7]  to-[#5f7499]   sm:w-5/6  lg:w-5/6        flex items-center justify-center	 gap-5      shadow-lg shadow-white-1000/50 border   p-10"
+            className=" bg-gradient-to-r from-[#034ca7]  to-[#5f7499] h-3/4   sm:w-5/6  lg:w-5/6        flex items-center justify-center	 gap-5      shadow-lg shadow-white-1000/50 border   p-10"
             style={{
-               
               borderRadius: "25px",
-              
-              height: "230px",
             }}
           >
-            <div  data-aos="fade-right"
-            data-aos-offset="300"
-            data-aos-delay="5000"
-            data-aos-duration="3000">
+            <div
+              data-aos="fade-right"
+              data-aos-offset="300"
+              data-aos-delay="5000"
+              data-aos-duration="3000"
+            >
               {" "}
               <img
                 src="https://www.rehabphysiolondon.com/wp-content/uploads/2019/06/Sports-Injuries.png "
@@ -156,17 +167,22 @@ const Services = () => {
                 alt=""
               />{" "}
             </div>
-            <div className="text-white "  data-aos="fade-down"
-            data-aos-offset="300"
-            data-aos-delay="5000"
-            data-aos-duration="3000">
-              <h2 className="tex-sm lg:text-2xl font-bold">Sporting Injuries & Pain</h2>
+            <div
+              className="text-white "
+              data-aos="fade-down"
+              data-aos-offset="300"
+              data-aos-delay="5000"
+              data-aos-duration="3000"
+            >
+              <h2 className="tex-sm lg:text-2xl font-bold">
+                Sporting Injuries & Pain
+              </h2>
               <p className="text-xs lg:text-[15px]">
                 Restoring normal function post-injury and pain management of
                 acute or chronic conditions
               </p>
               <div className="card-actions justify-end   mt-5">
-              <button
+                <button
                   className="btn btn-sm  "
                   style={{ backgroundColor: "#3E84DC" }}
                 >
@@ -175,38 +191,41 @@ const Services = () => {
               </div>
             </div>
           </div>
-           
           <div
-            className=" bg-gradient-to-r from-[#034ca7]  to-[#5f7499]   sm:w-5/6  lg:w-5/6      flex items-center justify-center	 gap-5      shadow-lg shadow-white-1000/50 border   px-10"
+            className=" bg-gradient-to-r from-[#034ca7]  to-[#5f7499] h-3/4   sm:w-5/6  lg:w-5/6        flex items-center justify-center	 gap-5      shadow-lg shadow-white-1000/50 border   p-10"
             style={{
-              
               borderRadius: "25px",
-             
-              height: "230px",
             }}
           >
-            <div  data-aos="fade-right"
-            data-aos-offset="300"
-            data-aos-delay="5000"
-            data-aos-duration="3000">
+            <div
+              data-aos="fade-right"
+              data-aos-offset="300"
+              data-aos-delay="5000"
+              data-aos-duration="3000"
+            >
               {" "}
               <img
-                src="https://www.rehabphysiolondon.com/wp-content/uploads/2019/06/Orthopaedic.png "
+                src="https://www.rehabphysiolondon.com/wp-content/uploads/2019/06/Orthopaedic.png"
                 className="w-96   lg:h-36  "
                 alt=""
               />{" "}
             </div>
-            <div className="text-white "  data-aos="fade-down"
-            data-aos-offset="300"
-            data-aos-delay="5000"
-            data-aos-duration="3000">
-              <h2 className="tex-sm lg:text-2xl font-bold ">Pre & Post Surgery</h2>
+            <div
+              className="text-white "
+              data-aos="fade-down"
+              data-aos-offset="300"
+              data-aos-delay="5000"
+              data-aos-duration="3000"
+            >
+              <h2 className="tex-sm lg:text-2xl font-bold">
+                Pre & Post Surgery
+              </h2>
               <p className="text-xs lg:text-[15px]">
                 A plan to prepare for surgery, optimize strength, function and
                 mobility afterwards.
               </p>
               <div className="card-actions justify-end   mt-5">
-              <button
+                <button
                   className="btn btn-sm  "
                   style={{ backgroundColor: "#3E84DC" }}
                 >
@@ -216,36 +235,38 @@ const Services = () => {
             </div>
           </div>
           <div
-            className="bg-gradient-to-r from-[#034ca7]  to-[#5f7499]    sm:w-5/6  lg:w-5/6      flex items-center justify-center	 gap-5      shadow-lg shadow-white-1000/50 border   px-10"
+            className=" bg-gradient-to-r from-[#034ca7]  to-[#5f7499] h-3/4   sm:w-5/6  lg:w-5/6        flex items-center justify-center	 gap-5      shadow-lg shadow-white-1000/50 border   p-10"
             style={{
-              
               borderRadius: "25px",
-              
-              height: "230px",
             }}
           >
-            <div  data-aos="fade-right"
-            data-aos-offset="300"
-            data-aos-delay="5000"
-            data-aos-duration="3000">
+            <div
+              data-aos="fade-right"
+              data-aos-offset="300"
+              data-aos-delay="5000"
+              data-aos-duration="3000"
+            >
               {" "}
               <img
-                src="https://www.rehabphysiolondon.com/wp-content/uploads/2020/09/Paediatrics-Icon.png "
+                src="https://www.rehabphysiolondon.com/wp-content/uploads/2020/09/Paediatrics-Icon.png"
                 className="w-96   lg:h-36  "
                 alt=""
               />{" "}
             </div>
-            <div className=" text-white"  data-aos="fade-down"
-            data-aos-offset="300"
-            data-aos-delay="5000"
-            data-aos-duration="3000">
+            <div
+              className="text-white "
+              data-aos="fade-down"
+              data-aos-offset="300"
+              data-aos-delay="5000"
+              data-aos-duration="3000"
+            >
               <h2 className="tex-sm lg:text-2xl font-bold">Paediatrics</h2>
               <p className="text-xs lg:text-[15px]">
                 Life enrichment activity program team of paediatric therapists
                 are here to support babies, children and teenagers aged 0 – 19.
               </p>
               <div className="card-actions justify-end   mt-5">
-              <button
+                <button
                   className="btn btn-sm  "
                   style={{ backgroundColor: "#3E84DC" }}
                 >
@@ -255,36 +276,38 @@ const Services = () => {
             </div>
           </div>
           <div
-            className="  bg-gradient-to-r from-[#034ca7]  to-[#5f7499]  sm:w-5/6  lg:w-5/6     flex items-center justify-center	 gap-5      shadow-lg shadow-white-1000/50 border   px-10"
+            className=" bg-gradient-to-r from-[#034ca7]  to-[#5f7499] h-3/4   sm:w-5/6  lg:w-5/6        flex items-center justify-center	 gap-5      shadow-lg shadow-white-1000/50 border   p-10"
             style={{
-              
               borderRadius: "25px",
-              
-              height: "230px",
             }}
           >
-            <div  data-aos="fade-right"
-            data-aos-offset="300"
-            data-aos-delay="5000"
-            data-aos-duration="3000">
+            <div
+              data-aos="fade-right"
+              data-aos-offset="300"
+              data-aos-delay="5000"
+              data-aos-duration="3000"
+            >
               {" "}
               <img
-                src="https://www.rehabphysiolondon.com/wp-content/uploads/2020/01/OtherTherapies.png "
+                src="https://www.rehabphysiolondon.com/wp-content/uploads/2020/01/OtherTherapies.png"
                 className="w-96   lg:h-36  "
                 alt=""
               />{" "}
             </div>
-            <div className="text-white "  data-aos="fade-down"
-            data-aos-offset="300"
-            data-aos-delay="5000"
-            data-aos-duration="3000">
-              <h2 className="tex-sm lg:text-2xl font-bold ">Other Therapies</h2>
+            <div
+              className="text-white "
+              data-aos="fade-down"
+              data-aos-offset="300"
+              data-aos-delay="5000"
+              data-aos-duration="3000"
+            >
+              <h2 className="tex-sm lg:text-2xl font-bold">Other Therapies
+</h2>
               <p className="text-xs lg:text-[15px]">
-                Learn more about Music Therapy, Speech and Language Therapy,
-                Neuropsychology and Occupational Therapy
+              Learn more about Music Therapy, Speech and Language Therapy, Neuropsychology and Occupational Therapy
               </p>
               <div className="card-actions justify-end   mt-5">
-              <button
+                <button
                   className="btn btn-sm  "
                   style={{ backgroundColor: "#3E84DC" }}
                 >
