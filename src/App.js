40c911d66f966116a420dd03ce5c3f7eb691e224
Sadnan-2019 <54290526+Sadnan-2019@users.noreply.github.com>
2022-12-12 23:40:30 +0600

    form landlord

--- src/App.js
+++ src/App.js
@@ -1,31 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
-import Nav from './Components/Nav/Nav';
-import Home from './Components/Home/Home';
-import Footer from './Components/Footer/Footer';
-import { Route, Routes } from 'react-router-dom';
-import Electrotherapy from './Components/Electrotherapy/Electrotherapy';
-import Physiotherapy from './Components/Physiotherapy/Physiotherapy';
-import ManualTherapy from './Components/ManualTherapy/ManualTherapy';
-import SportsInjuries from './Components/SportsInjuries/SportsInjuries';
-import Rehabilitation from './Components/Rehabilitation/Rehabilitation';
-import TherapeuticExercise from './Components/TherapeuticExercise/TherapeuticExercise';
-import KinesioTaping from './Components/KinesioTaping/KinesioTaping';
-import DryneedlingAcupuncture from './Components/DryneedlingAcupuncture/DryneedlingAcupuncture';
-import Topnav from './Components/Topnav/Topnav';
-import BackyPain from './Components/BackyPain/BackyPain';
-import NeckPain from './Components/NeckPain/NeckPain';
-import ShoulderPain from './Components/ShoulderPain/ShoulderPain';
-import ElbowPain from './Components/ElbowPain/ElbowPain';
-import KneePain from './Components/KneePain/KneePain';
-import MessengerCustomerChat from 'react-messenger-customer-chat/lib/MessengerCustomerChat';
+import logo from "./logo.svg";
+import "./App.css";
+import Nav from "./Components/Nav/Nav";
+import Home from "./Components/Home/Home";
+import Footer from "./Components/Footer/Footer";
+import { Route, Routes } from "react-router-dom";
+import Electrotherapy from "./Components/Electrotherapy/Electrotherapy";
+import Physiotherapy from "./Components/Physiotherapy/Physiotherapy";
+import ManualTherapy from "./Components/ManualTherapy/ManualTherapy";
+import SportsInjuries from "./Components/SportsInjuries/SportsInjuries";
+import Rehabilitation from "./Components/Rehabilitation/Rehabilitation";
+import TherapeuticExercise from "./Components/TherapeuticExercise/TherapeuticExercise";
+import KinesioTaping from "./Components/KinesioTaping/KinesioTaping";
+import DryneedlingAcupuncture from "./Components/DryneedlingAcupuncture/DryneedlingAcupuncture";
+import Topnav from "./Components/Topnav/Topnav";
+import BackyPain from "./Components/BackyPain/BackyPain";
+import NeckPain from "./Components/NeckPain/NeckPain";
+import ShoulderPain from "./Components/ShoulderPain/ShoulderPain";
+import ElbowPain from "./Components/ElbowPain/ElbowPain";
+import KneePain from "./Components/KneePain/KneePain";
+import MessengerCustomerChat from "react-messenger-customer-chat/lib/MessengerCustomerChat";
 // import TherapeuticExercise from './Components/TherapeuticExercise/TherapeuticExercise.JS';
 
 function App() {
   return (
     <div className=" ">
       <Topnav></Topnav>
-      <Nav/>
+      <Nav />
       {/* <Home/> */}
       <Routes>
         <Route path="/home" element={<Home />}></Route>
@@ -33,26 +33,33 @@ function App() {
         <Route path="/electrothyrapy" element={<Electrotherapy />}></Route>
         <Route path="/physiotherapy" element={<Physiotherapy />}></Route>
         <Route path="/manualtherapy" element={<ManualTherapy />}></Route>
-        <Route path="/sports-injuries" element={<SportsInjuries/>}></Route>
-        <Route path="/rehabilitation" element={<Rehabilitation/>}></Route>
-        <Route path="/therapeutic-exercise" element={<TherapeuticExercise></TherapeuticExercise>}></Route>
-        <Route path="/kinesio-taping-therapy" element={<KinesioTaping/>}></Route>
-        <Route path="/dryneedling-acupuncture" element={<DryneedlingAcupuncture/>}></Route>
-        <Route path="/back-pain" element={<BackyPain/>}></Route>
-        <Route path="/neck-pain" element={<NeckPain/>}></Route>
-        <Route path="/shoulder-pain" element={<ShoulderPain/>}></Route>
-        <Route path="/elbow-pain" element={<ElbowPain/>}></Route>
-        <Route path="/knee-pain" element={<KneePain/>}></Route>
+        <Route path="/sports-injuries" element={<SportsInjuries />}></Route>
+        <Route path="/rehabilitation" element={<Rehabilitation />}></Route>
+        <Route
+          path="/therapeutic-exercise"
+          element={<TherapeuticExercise></TherapeuticExercise>}
+        ></Route>
+        <Route
+          path="/kinesio-taping-therapy"
+          element={<KinesioTaping />}
+        ></Route>
+        <Route
+          path="/dryneedling-acupuncture"
+          element={<DryneedlingAcupuncture />}
+        ></Route>
+        <Route path="/back-pain" element={<BackyPain />}></Route>
+        <Route path="/neck-pain" element={<NeckPain />}></Route>
+        <Route path="/shoulder-pain" element={<ShoulderPain />}></Route>
+        <Route path="/elbow-pain" element={<ElbowPain />}></Route>
+        <Route path="/knee-pain" element={<KneePain />}></Route>
         {/* <Route path="/therapeutic-exercise" element={<TherapeuticExercise/>}></Route> */}
-         
+
         {/* <Route path="login" element={<Login />}></Route> */}
         {/* <Route path="/*" element={<FourZeroFour />} />  */}
       </Routes>
 
-
-
-      <Footer/>
-      <MessengerCustomerChat pageId="105187501050744" appId="500779031558701" />
+      <Footer />
+      <MessengerCustomerChat pageId="105187501050744" appId="690666775772229" />
     </div>
   );
 }
--- src/Components/Landing/Landingnew.js
+++ src/Components/Landing/Landingnew.js
@@ -69,10 +69,14 @@ const Landingnew = () => {
          <div className="hero-overlay bg-opacity-60"></div>
          <div className="hero-content text-center text-neutral-content">
            <div className="max-w-md">
-             <h1 className="mb-5 text-5xl font-bold"  data-aos="fade-right"
+             <h1 className="mb-5 text-3xl font-bold"  data-aos="fade-right"
               data-aos-offset="300"
               data-aos-delay="5000"
-              data-aos-duration="3000">Welcome to The LEAP Physiotherapy</h1>
+              data-aos-duration="3000">Welcome to The </h1>
+             <h1 className="mb-5 text-3xl font-bold"  data-aos="fade-right"
+              data-aos-offset="300"
+              data-aos-delay="5000"
+              data-aos-duration="3000">LEAP Physiotherapy </h1>
              <p className="mb-5 animate__animated animate__fadeInRight">
              Be seen by a Consultant expert in our physiotherapy center & get the best physiotherapy in the country.
                   We have state of the art equipments & qualified professionals to
