2827e1a87eb84d481172c003309c18071dd1fa4b
Sadnan-2019 <sadnan.wow@gmail.com>
2022-12-20 14:28:31 +0600

    services responcive

--- src/Components/Landing/Landingnew.js
+++ src/Components/Landing/Landingnew.js
@@ -61,29 +61,25 @@ const Landingnew = () => {
     
    <div className=" ">
    <div
-         className="hero min-h-screen"
-         style={{
-           backgroundImage: `url("https://i.ibb.co/QP33199/slider1.jpg")`,
-         }}
+         className="hero   text-red-700  sm:min-h-screen-0 lg:min-h-screen landingone"
+              
        >
          <div className="hero-overlay bg-opacity-o"></div>
          <div className="hero-content text-center text-neutral-content">
            <div className="max-w-md">
-             <h1 className="mb-5 text-4xl font-bold"  data-aos="fade-right"
+             <h1 className="mb-5 sm:text-2xl lg:text-4xl font-bold"  data-aos="fade-right"
               data-aos-offset="300"
               data-aos-delay="5000"
               data-aos-duration="3000">Welcome to The </h1>
-             <h1 className="mb-5 text-4xl font-bold"  data-aos="fade-right"
+             <h1 className="mb-5 sm:text-2xl lg:text-4xl font-bold"   data-aos="fade-right"
               data-aos-offset="300"
               data-aos-delay="5000"
               data-aos-duration="3000">LEAP Physiotherapy </h1>
              <p className="mb-5 animate__animated animate__fadeInRight">
-             Be seen by a Consultant expert in our physiotherapy center & get the best physiotherapy in the country.
-                  We have state of the art equipments & qualified professionals to
+                   We have state of the art equipments & qualified professionals to
                   heal your pain & suffering.
              </p>
-             <button className="btn btn-primary">Get Started</button>
-           </div>
+            </div>
          </div>
        </div>
    </div>
@@ -91,7 +87,7 @@ const Landingnew = () => {
      
       <div className="">
       <div
-         className="hero min-h-screen"
+         className="hero sm:min-h-fit text-red-700  sm:min-h-screen-0 lg:min-h-screen"
          style={{
            backgroundImage: `url("https://rebalancerehab.in/wp-content/uploads/2021/01/slide4-3.png")`,
          }}
@@ -99,13 +95,12 @@ const Landingnew = () => {
          <div className="hero-overlay bg-opacity-60"></div>
          <div className="hero-content text-center text-neutral-content">
            <div className="max-w-md">
-             <h1 className="mb-5 text-4xl font-bold">Passive Exercise</h1>
+             <h1 className="mb-5 sm:text-2xl lg:text-4xl font-bold">Passive Exercise</h1>
              <p className="mb-5">
                Some Physical Conditions Need to be Treated by Passive Exercise.
                Stroke Induced Paralysis is One of Them.
              </p>
-             <button className="btn btn-primary">Get Started</button>
-           </div>
+            </div>
          </div>
        </div>
      
@@ -113,7 +108,7 @@ const Landingnew = () => {
      
        <div className="">
        <div
-         className="hero min-h-screen"
+         className="hero sm:min-h-fit text-red-700  sm:min-h-screen-0 lg:min-h-screen"
          style={{
            backgroundImage: `url("https://content.jdmagicbox.com/comp/pune/s5/020pxx20.xx20.120804115010.l4s5/catalogue/dr-raut-s-shree-physiotherapy-and-slimming-center-dhayari-pune-physiotherapists-qgfipbx67v.jpg?clr=")`,
          }}
@@ -121,12 +116,12 @@ const Landingnew = () => {
          <div className="hero-overlay bg-opacity-60"></div>
          <div className="hero-content text-center text-neutral-content">
            <div className="max-w-md">
-             <h1 className="mb-5 text-4xl font-bold">Shortwave Diathermy</h1>
+             <h1 className="mb-5 sm:text-2xl lg:text-4xl font-bold">Shortwave Diathermy</h1>
              <p className="mb-5">
                 This Machine is Used to Give Deep Heat to the inflamed Tissues of the Body.
                 It Improves Circulation thus Enhances Healing Process.
              </p>
-             <button className="btn btn-primary">Get Started</button>
+             {/* <button className="btn btn-primary">Get Started</button> */}
            </div>
          </div>
        </div>
@@ -135,7 +130,7 @@ const Landingnew = () => {
      
       <div className="">
       <div
-         className="hero min-h-screen"
+         className="hero sm:min-h-fit text-red-700  sm:min-h-screen-0 lg:min-h-screen"
          style={{
            backgroundImage: `url("https://www.indica.today/wp-content/uploads/2022/06/History-of-Asana-and-Exercise-in-India.jpg")`,
          }}
@@ -143,12 +138,12 @@ const Landingnew = () => {
          <div className="hero-overlay bg-opacity-60"></div>
          <div className="hero-content text-center text-neutral-content">
            <div className="max-w-md">
-             <h1 className="mb-5 text-4xl font-bold">Active Exercise</h1>
+             <h1 className="mb-5 sm:text-2xl lg:text-4xl font-bold">Active Exercise</h1>
              <p className="mb-5">
                Acitive Exercise Improves Function,Increases Capability & Boosts Up 
                Physical & Mental Wellbeing.
              </p>
-             <button className="btn btn-primary">Get Started</button>
+             {/* <button className="btn btn-primary">Get Started</button> */}
            </div>
          </div>
        </div>
