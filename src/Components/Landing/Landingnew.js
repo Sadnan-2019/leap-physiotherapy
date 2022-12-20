4fd45f693ce5b02ce7e952fed7fae5e6be2a8cc7
Sadnan-2019 <sadnan.wow@gmail.com>
2022-12-20 15:37:49 +0600

    services responcive

--- src/Components/Landing/Landingnew.js
+++ src/Components/Landing/Landingnew.js
@@ -64,18 +64,18 @@ const Landingnew = () => {
          className="hero   text-red-700  sm:min-h-screen-0 lg:min-h-screen landingone"
               
        >
-         <div className="hero-overlay bg-opacity-o"></div>
+         <div className="lg:hero-overlay sm:bg-opacity-60"></div>
          <div className="hero-content text-center text-neutral-content">
            <div className="max-w-md">
-             <h1 className="mb-5 sm:text-2xl lg:text-4xl font-bold"  data-aos="fade-right"
+             <h1 className="lg:mb-5 sm:text-2xl lg:text-4xl font-bold"  data-aos="fade-right"
               data-aos-offset="300"
               data-aos-delay="5000"
               data-aos-duration="3000">Welcome to The </h1>
-             <h1 className="mb-5 sm:text-2xl lg:text-4xl font-bold"   data-aos="fade-right"
+             <h1 className="lg:mb-5 sm:text-2xl lg:text-4xl font-bold"   data-aos="fade-right"
               data-aos-offset="300"
               data-aos-delay="5000"
               data-aos-duration="3000">LEAP Physiotherapy </h1>
-             <p className="mb-5 animate__animated animate__fadeInRight">
+             <p className="lg:mb-5 animate__animated animate__fadeInRight">
                    We have state of the art equipments & qualified professionals to
                   heal your pain & suffering.
              </p>
@@ -87,12 +87,10 @@ const Landingnew = () => {
      
       <div className="">
       <div
-         className="hero sm:min-h-fit text-red-700  sm:min-h-screen-0 lg:min-h-screen"
-         style={{
-           backgroundImage: `url("https://rebalancerehab.in/wp-content/uploads/2021/01/slide4-3.png")`,
-         }}
+         className="hero sm:min-h-fit text-red-700  sm:min-h-screen-0 lg:min-h-screen landingtwo"
+          
        >
-         <div className="hero-overlay bg-opacity-60"></div>
+         <div className="lg:hero-overlay bg-opacity-60"></div>
          <div className="hero-content text-center text-neutral-content">
            <div className="max-w-md">
              <h1 className="mb-5 sm:text-2xl lg:text-4xl font-bold">Passive Exercise</h1>
